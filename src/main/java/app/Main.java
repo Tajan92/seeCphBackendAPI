4c3500ed08d64e470299bf14482ac1204ee514e6
Theis <theisrud@gmail.com>
2026-08-25 16:22:38 +0200

    Initial commit

---
+++ src/main/java/app/Main.java
@@ -0,0 +1,16 @@
+package app;
+
+import app.config.HibernateConfig;
+import jakarta.persistence.EntityManagerFactory;
+
+public class Main {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = HibernateConfig.getEntityManagerFactory();
+
+
+
+        // Close the database connection:
+        emf.close();
+    }
+}
+
