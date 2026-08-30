f974e97ee0e7454e6cf1882c37e6cba3839122d7
Theis <theisrud@gmail.com>
2026-08-30 20:34:09 +0200

    ENUMS done for now, may be adding more categories

--- src/main/java/app/enums/EventCategory.java
+++ src/main/java/app/enums/EventCategory.java
@@ -1,6 +1,5 @@
 package app.enums;
 
-// TODO: Make more categories or get rid of categories ENUM ??
 public enum EventCategory {
-    FREE, MUSIC, SPORT, ART,
+    MUSIC, SPORT, ART, COMPETITION, TECHNOLOGY, FOOD, LIFESTYLE, HEALTH, COMMUNITY, CAREER,
 }
