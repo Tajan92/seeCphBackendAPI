544a41c59ce2ecfa6a69dcf71ab8358a0f3373d8
Theis <theisrud@gmail.com>
2026-09-11 15:27:17 +0200

    Added test for AdvertDAO for basic CRUD operations

--- src/test/java/app/daos/AdvertDAOTest.java
+++ src/test/java/app/daos/AdvertDAOTest.java
@@ -2,13 +2,23 @@ package app.daos;
 
 import app.config.HibernateTestConfig;
 import app.entities.Advert;
+import app.entities.Event;
 import app.entities.users.User;
+import app.enums.AddPlacement;
+import app.enums.EventCategory;
+import app.exceptions.DatabaseException;
 import app.utils.TestDataCreator;
 import jakarta.persistence.EntityManagerFactory;
 import org.junit.jupiter.api.*;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.Map;
+import java.util.Set;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.*;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -35,21 +45,54 @@ class AdvertDAOTest {
 
     @Test
     void create() {
+        Advert advert = Advert.builder()
+                .addPlacement(AddPlacement.FRONTPAGEHIGHLIGHT)
+                .price(150.00)
+                .startDate(LocalDate.of(2026, 10, 1))
+                .endDate(LocalDate.of(2026, 10, 31))
+                .build();
+
+        Advert advertCreated = advertDAO.create(advert);
+        assertThat(advertCreated.getAdvertId(), notNullValue());
+
+        Advert advertFetched = advertDAO.read(advertCreated.getAdvertId());
+        assertThat(advertFetched.getPrice(), equalTo(150.00));
+        assertThat(advertFetched.getAdvertId(), is(3));
     }
 
     @Test
     void read() {
+        Advert advert = adverts.get("advert");
+        Advert advertFetched = advertDAO.read(advert.getAdvertId());
+        assertThat(advertFetched.getPrice(), equalTo(205.00));
+        assertThat(advertFetched.getAdvertId(), is(advert.getAdvertId()));
     }
 
     @Test
     void readAll() {
+        Set<Advert> allAdverts = advertDAO.readAll();
+        assertThat(allAdverts, hasSize(2));
+        assertThat(allAdverts, containsInAnyOrder(adverts.values().toArray()));
     }
 
     @Test
     void update() {
+        Advert advert = adverts.get("advert");
+        advert.setPrice(50.00);
+
+        Advert fetchedAdvert = advertDAO.update(advert);
+
+        assertThat(fetchedAdvert.getAdvertId(), is(advert.getAdvertId()));
+        assertThat(fetchedAdvert.getPrice(), is(50.00));
+        assertThat(fetchedAdvert.getAdvertId(), is(advert.getAdvertId()));
     }
 
     @Test
     void delete() {
+        Advert advert = adverts.get("advert");
+
+        boolean deletedAdvert = advertDAO.delete(advert);
+        assertThat(deletedAdvert, is(true));
+        assertThrows(DatabaseException.class, () -> advertDAO.read(advert.getAdvertId()));
     }
 }
