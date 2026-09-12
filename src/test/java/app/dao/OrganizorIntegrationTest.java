713d7c3a0da62fa280d3f39f8cf6a4ad18d2a484
Theis <theisrud@gmail.com>
2026-09-12 09:52:21 +0200

    Added last test for organizer integration test

--- src/test/java/app/dao/OrganizorIntegrationTest.java
+++ src/test/java/app/dao/OrganizorIntegrationTest.java
@@ -26,7 +26,6 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class OrganizorIntegrationTest {
     private final EntityManagerFactory emf = HibernateTestConfig.getEntityManagerFactory();
-
     private UserDAO userDAO;
     private AdvertDAO advertDAO;
     private EventDAO eventDAO;
@@ -56,11 +55,6 @@ public class OrganizorIntegrationTest {
         userDAO = new UserDAO(emf);
         eventDAO = new EventDAO(emf);
         advertDAO = new AdvertDAO(emf);
-    }
-
-    @AfterAll
-    void tearDown() {
-        emf.close();
     }
 
     @Test
@@ -103,6 +97,7 @@ public class OrganizorIntegrationTest {
 
         Organizer savedOrganizer = (Organizer) userDAO.create(organizer);
 
+        assertThat(event.getOrganizer(), is(savedOrganizer));
         assertThat(savedOrganizer.getUserId(), notNullValue());
         assertThat(savedOrganizer.getAdverts().size(), is(1));
         assertThat(savedOrganizer.getEvents().size(), is(1));
@@ -147,10 +142,51 @@ public class OrganizorIntegrationTest {
         assertThat(deleted, is(true));
 
         DatabaseException ex = assertThrows(DatabaseException.class, () -> advertDAO.read(advertId));
-        assertThat(ex.getMessage(), is("Advert not found with id: 3"));
+        assertThat(ex.getMessage(), is("Advert not found with id: "+advertId));
 
         DatabaseException ex2 = assertThrows(DatabaseException.class, () -> eventDAO.read(eventId));
-        assertThat(ex2.getMessage(), is("Event not found with id: 3"));
+        assertThat(ex2.getMessage(), is("Event not found with id: "+eventId));
+
+
+    }
+
+    @Test
+    void deleteAdvertAndEvent() {
+        Advert advert = Advert.builder()
+                .addPlacement(AddPlacement.FRONTPAGEHIGHLIGHT)
+                .price(150.00)
+                .startDate(LocalDate.of(2026, 10, 1))
+                .endDate(LocalDate.of(2026, 10, 31))
+                .build();
+
+        organizer.addAdvert(advert);
+
+        Event event = Event.builder()
+                .title("Copenhagen Marathon")
+                .description("Run through Copenhagen with busy streets")
+                .categories(Set.of(EventCategory.SPORT))
+                .startTime(LocalTime.of(16, 0))
+                .endTime(LocalTime.of(18, 30))
+                .startDates(Set.of(LocalDate.of(2026, 10, 18)))
+                .price(100.00)
+                .location("Copenhagen")
+                .build();
+
+        organizer.addEvent(event);
+
+        Organizer createdOrganizer = (Organizer) userDAO.create(organizer);
+
+        Integer advertId = createdOrganizer.getAdverts().iterator().next().getAdvertId();
+        Integer eventId = createdOrganizer.getEvents().iterator().next().getEventId();
+        Advert fetchedAdvert = advertDAO.read(advertId);
+        Event fetchedEvent = eventDAO.read(eventId);
+
+        createdOrganizer.getAdverts().remove(fetchedAdvert);
+        createdOrganizer.getEvents().remove(fetchedEvent);
+
+        Organizer updatedOrganizer = (Organizer) userDAO.update(organizer);
+        assertThat(updatedOrganizer.getAdverts().size(), is(0));
+        assertThat(updatedOrganizer.getEvents().size(), is(0));
     }
 
 }
